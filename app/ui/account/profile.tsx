83aa688325a1d4283ac7bc16722d352648ebfd5f
Vincent Minutella <andrew.minutella88@gmail.com>
2024-02-23 15:29:35 -0800

    update for public profile

--- app/ui/account/profile.tsx
+++ app/ui/account/profile.tsx
@@ -51,16 +51,6 @@ export default function Profile( { user } : { user: User | undefined } ) {
       <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-600" type="tel" name="phone" defaultValue={user?.phone} />
     </div>
   </div>
-  <div className="md:flex md:items-center mb-6">
-    <div className="md:w-1/3">
-    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-        Password:
-      </label>
-    </div>
-    <div className="md:w-2/3">
-      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-600" type="password" name="password" defaultValue={user?.password} />
-    </div>
-  </div>
   <div className="flex items-center justify-center mb-6">
       <button type="submit" className="shadow bg-blue-600 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Submit</button>
   </div>
