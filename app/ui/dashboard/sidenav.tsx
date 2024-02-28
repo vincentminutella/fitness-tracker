5381f357248f4e35673ad0f39586978d1ceaac36
Vincent Minutella <andrew.minutella88@gmail.com>
2024-02-27 17:20:03 -0800

    remove sign out button mobile

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -19,7 +19,7 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form
+        <form className='hidden md:block'
           action={async () => {
             'use server';
             await signOut();
